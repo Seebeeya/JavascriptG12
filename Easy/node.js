70ad1fec1f33e28b975995f9989830f080e01478
Brittany Marie Shelby Adamson <marie.s.98@hotmail.com>
2022-09-27 01:01:45 -0400

    Updated Easy JS

---
+++ Easy/node.js
@@ -0,0 +1,10 @@
+const students = [];
+students[1]= "Degrass, Evan, 25";
+students[2]= "Adamson, Shelby, 23";
+students[3]= "Cameron, Justin, 24";
+function greet() {
+    return 'Hello, my name is Shelby Adamson and I am 23 years old';
+  }
+  console.log(greet());
+  console.log(students[2]);
+  document.write(students[2]);
