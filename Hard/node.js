94dab1f5082ced9648c9274c62c0b58876f20980
Brittany Marie Shelby Adamson <marie.s.98@hotmail.com>
2022-09-26 23:39:07 -0400

    first commit

---
+++ Hard/node.js
@@ -0,0 +1,13 @@
+var tomBmi, h1, m1, jerryBmi, h2, m2;
+
+h1 = 9;
+m1 = 8;
+
+h2 = 10;
+m2 = 45;
+
+
+tomBmi = m1 / h1 * 2;
+jerryBmi = m2 / h2 * 2;
+
+console.log(`Is Tom’s BMI higher than Jerry’s? ${tomBmi > jerryBmi}`);
