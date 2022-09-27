b6e5e7a140e1df2cdb77e40f9c9e7fc7a0a2075d
Brittany Marie Shelby Adamson <marie.s.98@hotmail.com>
2022-09-27 00:26:27 -0400

    Updated Medium JS

--- Medium/node.js
+++ Medium/node.js
@@ -1,5 +1,47 @@
-let prompt = ("Please Enter a Number Between One and Twelve")
-var months = {1:'January', 2:'February', 3:'March', 4:'April', 5:'May', 6:'June', 7:'July', 8:'August', 9:'September', 10:'October', 11:'November', 12:'December'
-};
-console.log(months[1])
+let number = prompt('Enter a Number Between One and Twelve')
+
+if (number == 1) {
+    alert('January');
+  } 
+  else if (number == 2 ) {
+    alert('February');
+  }
+else if (number == 3 ) {
+  alert('March');
+}
+else if (number == 4 ) {
+    alert('April');
+  }
+  else if (number == 5 ) {
+    alert('May');
+  }
+  else if (number == 6 ) {
+    alert('June');
+  }
+  else if (number == 7 ) {
+    alert('July');
+  }
+  else if (number == 8 ) {
+    alert('August');
+  }
+  else if (number == 9 ) {
+    alert('September');
+  }
+  else if (number == 10 ) {
+    alert('October');
+  }
+  else if (number == 11 ) {
+    alert('November');
+  }
+  else if (number == 12 ) {
+    alert('December');
+  }
+  else
+  alert('Invalid Number');
+
+  
+ 
+
+
+
 
